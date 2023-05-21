fc9114e23efce05b1360ea33dfa5dd8ba2c594d4
pereira299 <lucasper057@gmail.com>
2023-05-20 23:00:17 -0300

    upload

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,49 @@
+import { randomUUID } from 'crypto'
+import { FastifyInstance } from 'fastify'
+import { createWriteStream } from 'fs'
+import { extname, resolve } from 'path'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+
+const pump = promisify(pipeline)
+
+export async function uploadRoutes(app: FastifyInstance) {
+  app.post('/upload', async (req, res) => {
+    const upload = await req.file({
+      limits: {
+        fileSize: 1024 * 1024 * 5, // 5mb
+      },
+    })
+
+    if (!upload) {
+      return res.status(400).send({
+        message: 'Erro ao fazer upload do arquivo',
+      })
+    }
+
+    const mimeTypeRegex = /^(image|video)\/[a-zA-z]+/
+    const isValidMimeType = mimeTypeRegex.test(upload.mimetype)
+    if (!isValidMimeType) {
+      return res.status(400).send({
+        message: 'Tipo de arquivo inválido',
+      })
+    }
+
+    const fileId = randomUUID()
+    const extFile = extname(upload.filename)
+
+    const fileName = `${fileId}${extFile}`
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads', fileName),
+    )
+
+    await pump(upload.file, writeStream)
+
+    const fullUrl = `${req.protocol}://${req.hostname}:${process.env.PORT}/uploads/${fileName}`
+
+    return {
+      url: fullUrl,
+    }
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,24 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import staticFiles from '@fastify/static'
 import * as dotenv from 'dotenv'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 dotenv.config()
 
 const app = fastify()
 app.register(cors, {
   origin: true,
+})
+app.register(multipart)
+app.register(staticFiles, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
 })
 app.register(jwt, {
   secret: 'supersecret',
@@ -19,6 +28,7 @@ app.get('/', async (req, res) => {
 })
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
